07ae5fcb2dbd1a5898864ab25c60dbde396a33e9
Роман Прокопенко <prokopenkoroman88@gmail.com>
2022-02-24 09:02:44 +0200

    adding the splines...

--- js/BezierEditor.js
+++ js/BezierEditor.js
@@ -1,5 +1,6 @@
 import Tag from './common/tag-editor.js';
-import { BezierPoint, BezierCurve, BezierFigure, BezierCanvas } from './canvas/BezierFigure.js';
+import { Point, Spline, BezierPoint, BezierCurve, BezierFigure, BezierCanvas } from './canvas/BezierFigure.js';
+import PixelColor from './canvas/PixelColor.js';
 
 const mouseMv=0, mouseDn=1, mouseUp=2;
 const btnLeft=0, btnRight=2;//from https://developer.mozilla.org/ru/docs/Web/API/Element/mousedown_event
@@ -27,6 +28,8 @@ export default class BezierEditor{
 		this.mode=modeArrow;
 		this.iter=0;
 		this.args=[{},{},{}];
+		this.currPoint=null;
+		this.currSpline=null;
 	}
 
 	initPanel(){
@@ -41,22 +44,28 @@ export default class BezierEditor{
 		  .up();
 
 		this.btnArrow.currHTMLTag.addEventListener('click', function(){
+			console.log('editor.mode ?');
+			console.log('editor.mode ', BezierEditor.editor.mode);
 			BezierEditor.editor.mode=modeArrow;
+			console.log('editor.mode ', BezierEditor.editor.mode);
 		});
 
 		this.btnAddLayer.currHTMLTag.addEventListener('click', function(){
 			BezierEditor.editor.mode=modeAddLayer;
+			console.log('editor.mode ', BezierEditor.editor.mode);
 		});
 
 		this.btnAddPoint.currHTMLTag.addEventListener('click', function(){
 			BezierEditor.editor.mode=modeAddPoint;
 			BezierEditor.editor.iter=-1;
+			console.log('editor.mode ', BezierEditor.editor.mode);
 		});
 
 		this.btnFill.currHTMLTag.addEventListener('click', function(){
 			BezierEditor.editor.canvas.ctx.fillStyle=BezierEditor.editor.inpFill.currHTMLTag.value;
 			BezierEditor.editor.canvas.ctx.fill();
 			BezierEditor.editor.mode=modeArrow;//?
+			console.log('editor.mode ', BezierEditor.editor.mode);
 		});
 	}
 
@@ -72,18 +81,119 @@ export default class BezierEditor{
 		});
 	}
 
+	addPoint(x,y){
+		this.currPoint = new Point(x,y);
+		this.canvas.points.push(this.currPoint);
+		return this.currPoint;
+	}
+
+	addSpline(args){
+		let oldPoint = this.currPoint;
+		let newPoint = this.addPoint(args[2].x, args[2].y);
+
+		this.currSpline = new Spline([oldPoint, newPoint]);//newPoint==this.currPoint
+		this.canvas.splines.push(this.currSpline);
+
+		this.currSpline.leverPoint.push( new Point(args[0].x, args[0].y) );//after
+		this.currSpline.leverPoint.push( new Point(args[1].x, args[1].y) );//before
+		return this.currSpline;
+	}
+
 	onMouse(event,kak){
-		console.log('kak'+kak);
-		console.log(event);
+		//console.log('kak'+kak);
+		//console.log(event);
 
 		let x=event.offsetX;
 		let y=event.offsetY;
 		this.button = event.button;
-		console.log(x,y);
+		//console.log(x,y);
+
+		switch (this.mode) {
+			case modeArrow:
+			{
+
+
+				switch(kak){
+				case mouseDn:{
+					//
+					let iPoint;
+					iPoint = this.canvas.findPointByCoords(x,y);
+
+/*
+что мы находим?
+
+controlPoint        repaint 1,2 Spline by controlPoint
+leverPoint          repaint 1 Spline by leverPoint
+
+
+
+points
+
+splines
+
+curves/figures
+
+
+
+
+figure splines:[]  points:[]
+object figures:[] objects?:[]
+layer objects:[]
+
+
+
+
+*/
+					console.log('iPoint=',iPoint);
+
+					if(iPoint>=0)
+						this.currPoint = this.canvas.points[iPoint]
+					else
+						this.currPoint = null;
+
+					if(this.currPoint){
+					console.log('Point[].x,y=',this.currPoint.x, this.currPoint.y);
+						this.canvas.ctx.beginPath();
+						this.canvas.ctx.arc(this.currPoint.x,this.currPoint.y, 5, 0, Math.PI*2, true);
+						this.canvas.ctx.stroke();
+						this.refresh();
+					};
+
+
+
+
+				}; break;
+				case mouseUp:{
+
+					if(this.currPoint){
+
+						this.currPoint.x=x;
+						this.currPoint.y=y;
+
+						//fill by white?
+						this.refresh();
+
+						//this.currPoint=null;
+					};
+
+
+				}; break;
+				};//switch
+
+
+
+			}; break;
+			case modeAddLayer:
+			{
+
+			}; break;
+			case modeAddPoint:
+			{
 
 		if(kak==mouseDn){
 
 			if(this.iter==-1){
+				this.addPoint(x,y);
 				this.canvas.ctx.beginPath();
 				this.canvas.ctx.moveTo(x, y);
 
@@ -95,6 +205,7 @@ export default class BezierEditor{
 				this.iter = (this.iter+1)%3;
 
 				if(this.iter==0){
+					this.addSpline(this.args);
 					this.canvas.ctx.bezierCurveTo(this.args[0].x, this.args[0].y,  this.args[1].x, this.args[1].y,   this.args[2].x, this.args[2].y  );
 
 					//закрашивать надо в конце
@@ -106,12 +217,35 @@ export default class BezierEditor{
 					this.canvas.ctx.stroke();
 
 
-					this.canvas.refreshImageData();
+					this.refresh();
+					this.paintSpline(this.currSpline);
 
 				};
 			};
 		};
 
+			}; break;
+			default:
+			{
+			}; break;
+		};//switch mode
+
+	}
+
+	paintSpline(spline){
+		//
+		console.log('spline=');
+		console.log(spline);
+		let aDot=spline.toArray();
+		let clr =  new PixelColor('#22ffee');
+		this.canvas.paintBezier3(aDot,clr);
+		this.canvas.paintBezier(aDot,clr);
+		this.canvas.put();
+	}
+
+	refresh(){
+		//this.canvas.put();
+		this.canvas.refreshImageData();
 	}
 
 }
--- js/canvas/BezierFigure.js
+++ js/canvas/BezierFigure.js
@@ -2,6 +2,58 @@ import CustomCanvas from './CustomCanvas.js';
 import VirtualCanvas from './VirtualCanvas.js';
 import RealCanvas from './RealCanvas.js';
 
+const cControlPoint=1, cBeforePoint=2, cAfterPoint=3, cRotatePoint=4;
+
+/*
+
+{x, y, kind=1}
+{x, y, kind=3}
+
+{x, y, kind=2}
+{x, y, kind=1}
+{x, y, kind=3}
+
+{x, y, kind=2}
+{x, y, kind=1}
+
+
+{control:{x,y},after:{x,y}}
+{control:{x,y},before:{x,y},after:{x,y}}
+{control:{x,y},before:{x,y}}
+
+
+*/
+
+class Point {
+	constructor(x,y){
+		this.x = x;
+		this.y = y;
+	}
+	shift(dx,dy){
+		this.x+=dx;
+		this.y+=dy
+	}
+	distance(x,y){
+		return Math.sqrt(Math.pow(x-this.x,2) + Math.pow(y-this.y,2));
+	}
+
+};
+
+
+class Spline {
+	constructor(points){
+		this.controlPoint=points;//point0,point1
+		this.leverPoint=[];
+		this.width = 1;
+		this.color = '#000';
+	}
+
+	toArray(){
+		return [this.controlPoint[0],this.leverPoint[0],this.leverPoint[1],this.controlPoint[1]];
+	}
+};
+
+
 function createPoint(x,y){
 	return {x:x,y:y};
 };
@@ -71,18 +123,29 @@ class BezierCanvas extends RealCanvas{
 
 	init(selector){
 		super.init(selector);
+		console.log('BezierCanvas.init('+selector+')');
+		console.log(this.canvas);
+		this.points = [];
+		this.splines = [];
 	}
 
 
+	findPointByCoords(x,y){
+		for(let i=0; i<this.points.length; i++){
+			if(this.points[i].distance(x,y)<5)
+				return i;
+		};
+		return -1;
+	}
 
-	findBezierPoint(aDot, coef){//interim point
+	findInterimBezierPoint(aDot, coef){//interim point
 		function middle(dot1,dot2,coef){
 			return {
 				x: dot1.x + (dot2.x-dot1.x)*coef,
 				y: dot1.y + (dot2.y-dot1.y)*coef,
 			};
 		};
-		//let aPnt=[{x,y},{x,y},{x,y}, {x,y},{x,y}, {x,y}];
+/*		//let aPnt=[{x,y},{x,y},{x,y}, {x,y},{x,y}, {x,y}];
 		let aPnt=[{},{},{}, {},{}, {}];
 
 		aPnt[0]=middle(aDot[0], aDot[1], i/c);
@@ -96,18 +159,39 @@ class BezierCanvas extends RealCanvas{
 		aPnt[5]=middle(aPnt[3], aPnt[4], i/c);
 
 		return aPnt[5];
+*/
+
+		let aPnt=[{}, {},{}, {},{},{}, {},{},{},{}];
+		aPnt[6]=aDot[0];
+		aPnt[7]=aDot[1];
+		aPnt[8]=aDot[2];
+		aPnt[9]=aDot[3];
+
+		aPnt[3]=middle(aPnt[6], aPnt[7], coef);
+		aPnt[4]=middle(aPnt[7], aPnt[8], coef);
+		aPnt[5]=middle(aPnt[8], aPnt[9], coef);
+
+		aPnt[1]=middle(aPnt[3], aPnt[4], coef);
+		aPnt[2]=middle(aPnt[4], aPnt[5], coef);
+		
+		aPnt[0]=middle(aPnt[1], aPnt[2], coef);
+
+		return aPnt[0];
 	}
 
-
-
-
+	/*paintPoint(point,rgba){
+			let x=Math.round(point.x);
+			let y=Math.round(point.y);
+			this.setRGB(x,y,rgba);//canvas
+		};*/
 
 	paintBezier(aDot, color){//PixelColor color
 
+		let cnv=this;
 		function paintPoint(point){
 			let x=Math.round(point.x);
 			let y=Math.round(point.y);
-			this.canvas.setRGB(x,y,rgba);
+			cnv.setRGB(x,y,rgba);
 		};
 
 
@@ -118,11 +202,14 @@ class BezierCanvas extends RealCanvas{
 
 
 		let oldPoint, newPoint=aDot[0];
-		let c=200;
+		let c=0;
+		for(let i=1;i<=3;i++)
+			c+=aDot[i-1].distance(aDot[i].x,aDot[i].y);
+		//c=c/2;//4
 		//=Math.hypot(aDot);//
-		for(let i=0; i<=c; i++){
+		for(let i=1; i<=c; i++){
 			oldPoint = newPoint;
-			newPoint = this.findBezierPoint(aDot, i/c);
+			newPoint = this.findInterimBezierPoint(aDot, i/c);
 			//console.log(i );
 			paintPoint(newPoint);
 			//console.log(aPnt);
@@ -130,6 +217,112 @@ class BezierCanvas extends RealCanvas{
 
 		//this.canvas.put();
 	}
+
+//http://www.sopromat.info/cubic-equations-online-048.html
+//https://naurok.com.ua/kubichni-rivnyannya-metod-kardano-metod-vieta-89259.html
+
+
+paintBezier3(aDot, color){//bezier_cubic
+var iii=0;
+	function discr(p,q){
+		let D = Math.pow(p/3, 3) + Math.pow((q)/2 ,2);
+
+		if(iii<1000){
+			//console.log('p,q,D: ',p,q,D);
+			iii++;
+		};
+		if(D<0)D=Math.abs(D);//???
+		let rD = Math.pow(D,1/2);// = -\/D
+		if(iii<1000){
+			//console.log('rD: ',rD, Math.pow(-27, 1/3));
+
+		};
+		let arg1=-q/2+rD, sgn1=1; if(arg1<0){ sgn1=-1; arg1=-arg1;  };
+		let arg2=-q/2-rD, sgn2=1; if(arg2<0){ sgn2=-1; arg2=-arg2;  };
+		let z1 = sgn1*Math.pow(arg1, 1/3) + sgn2*Math.pow(arg2, 1/3);
+
+		return z1;
+	};
+
+function grad(rgba1,rgba2,coef){
+	let res=[0,0,0,255];
+	coef = (Math.sin(coef*Math.PI-Math.PI/2)+1)/2;//0..1
+	for(let z=0; z<3; z++)
+		res[z] =   Math.round(rgba1[z] + (rgba2[z]  - rgba1[z])*coef);
+	return res;
+};
+
+
+let rgba=color.toArray();//[255,200,100,255]
+//a*x^3 + b*x^2 + cx + d = 0
+//x=iter  a!=0
+
+let ax =   aDot[3].x - 3*aDot[2].x + 3*aDot[1].x -   aDot[0].x;
+let bx =               3*aDot[2].x - 6*aDot[1].x + 3*aDot[0].x;
+let cx =                             3*aDot[1].x - 3*aDot[0].x;
+//let d =                                             aDot[0];
+
+let rx = bx/ax;
+let sx = cx/ax;
+
+let px = (3*sx-rx*rx)/3;
+let qx = 2*rx*rx*rx/27 - rx*sx/3; // + t!!
+//-----------
+let ay =   aDot[3].y - 3*aDot[2].y + 3*aDot[1].y -   aDot[0].y;
+let by =               3*aDot[2].y - 6*aDot[1].y + 3*aDot[0].y;
+let cy =                             3*aDot[1].y - 3*aDot[0].y;
+//let d =                                             aDot[0];
+
+let ry = by/ay;
+let sy = cy/ay;
+
+let py = (3*sy-ry*ry)/3;
+let qy = 2*ry*ry*ry/27 - ry*sy/3; // + t!!
+
+
+
+
+//for(let y=60; y<120; y++){
+for(let y=0; y<100; y++){
+
+	let dy=aDot[0].y-y;
+	let ty = dy/ay;
+
+	let itery = discr(py,qy+ty);	
+//	console.log('itery='+itery, ty, dy, ay);
+
+	//for(let x=30; x<570; x++){
+	for(let x=0; x<100; x++){
+
+		let dx=aDot[0].x-x;
+		let tx = dx/ax;
+		let iterx = discr(px,qx+tx);
+
+
+//console.log('   iterx='+iterx, x);
+//this.setRGB(Math.round(y/10+x) ,100+Math.round(iterx*100),[255,0,255,255]);
+//this.setRGB(Math.round(iterx*100) ,100+Math.round(itery*100),[255,0,255,255]);
+if(x=0){
+this.setRGB(Math.round(100+y) ,100+Math.round(itery*100),[255,0,255,255]);
+};
+
+
+
+		//console.log('iterx='+iterx);
+		if(Math.abs(iterx - itery)<31){
+			//console.log('iterx='+iterx, x);
+			let rgb5=grad(this.getRGB(x,y), rgba , 1/Math.pow(Math.abs(iterx - itery)+1,4)  );
+			this.setRGB(x,y,rgb5);
+		}
+
+
+	};//x
+};//y
+
+
+
+
+}
 
 
 
@@ -141,6 +334,6 @@ class BezierCanvas extends RealCanvas{
 };
 
 
-export { BezierPoint, BezierCurve, BezierFigure, BezierCanvas };
+export { Point, Spline, BezierPoint, BezierCurve, BezierFigure, BezierCanvas };
 
 
